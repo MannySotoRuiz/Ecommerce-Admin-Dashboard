38c3193c061c086c67d81b020dadfd3610377b17
MannySotoRuiz <sotoruiz.emmanuel@gmail.com>
2024-09-22 23:27:01 -0400

    updated metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,29 @@
-import { ClerkProvider } from '@clerk/nextjs'
-import { Inter } from 'next/font/google'
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
 
-import { ModalProvider } from '@/providers/modal-provider'
-import { ToastProvider } from '@/providers/toast-provider'
-import { ThemeProvider } from '@/providers/theme-provider'
+import { ModalProvider } from "@/providers/modal-provider";
+import { ToastProvider } from "@/providers/toast-provider";
+import { ThemeProvider } from "@/providers/theme-provider";
 
-import './globals.css'
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Dashboard',
-  description: 'E-Commerce Dashboard',
-}
+  title: "E-Commerce Dashboard",
+  description: "E-Commerce Dashboard",
+};
 
 export default async function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          <ThemeProvider 
-            attribute="class" 
-            defaultTheme="system" 
-            enableSystem
-          >
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             <ToastProvider />
             <ModalProvider />
             {children}
@@ -35,5 +31,5 @@ export default async function RootLayout({
         </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
